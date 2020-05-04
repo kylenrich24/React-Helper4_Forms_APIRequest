96e9e7953f34fabf61f5c8937ff11effa96e3ef4
Kyle <kylennardsy@hotmail.com>
2020-05-04 00:36:11 -0400

    Updates

---
+++ precache-manifest.53f1399268436b7c2c6b576a5cbe081c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f69510068c5c408af6cc7f69c9c33028",
+    "url": "/React-Helper4_Forms_APIRequest/index.html"
+  },
+  {
+    "revision": "eac7f8450723ff167e1d",
+    "url": "/React-Helper4_Forms_APIRequest/static/css/main.c11a3498.chunk.css"
+  },
+  {
+    "revision": "7d3b5d89d8ebe4604dec",
+    "url": "/React-Helper4_Forms_APIRequest/static/js/2.b042701d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Helper4_Forms_APIRequest/static/js/2.b042701d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac7f8450723ff167e1d",
+    "url": "/React-Helper4_Forms_APIRequest/static/js/main.b05a9480.chunk.js"
+  },
+  {
+    "revision": "a4c0e716a5689e86b07c",
+    "url": "/React-Helper4_Forms_APIRequest/static/js/runtime-main.f854aa89.js"
+  }
+]);
